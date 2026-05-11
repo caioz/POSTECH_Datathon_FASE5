62cca10725b526efa29feafa14cf81f7985d67ce
Caioz <44296556+caioz@users.noreply.github.com>
2026-05-11 20:12:05 -0300

    Update Projeto_passos_magicos.py

--- Projeto_passos_magicos.py
+++ Projeto_passos_magicos.py
@@ -92,9 +92,9 @@ with aba1:
 
     st.subheader("Indicadores Estratégicos")
 
-    media22 = df22.filter(like="%INDE%").mean().values[0]
-    media23 = df23.filter(like="%INDE%").mean().values[0]
-    media24 = df24.filter(like="%INDE%").mean().values[0]
+    media22 = df22.filter(like="INDE 22").mean().values[0]
+    media23 = df23.filter(like="INDE 23").mean().values[0]
+    media24 = df24.filter(like="INDE 24").mean().values[0]
 
     crescimento = ((media24 - media22) / media22) * 100
 
